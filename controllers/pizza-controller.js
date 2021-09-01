eaa81197112d519d9b8d22aa6a7635e98419603c
TraBee <tra8brown@gmail.com>
2021-09-01 14:33:32 -0600

    added routes, and post apis

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,51 @@
+const { Pizza } = require('../models');
+
+//method for finding all pizza data and specific pizza by it's _id.
+const pizzaController = {
+    //get all pizzas GET/api/pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    //get one pizza by id
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+            .then(dbPizzaData => {
+                //If no pizza is found, send 404
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    //createPizza //POST /api/pizzas
+    createPizza({ body }, res) {
+        Pizza.create(body)
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.status(400).json(err));
+    },
+    //update pizza by id //PUT /api/pizzas/:id
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+}
+
+
+module.exports = pizzaController;
