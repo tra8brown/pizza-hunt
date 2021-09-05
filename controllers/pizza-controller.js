45cd32571ff29ce4a89299ace3044d3537ebb263
TraBee <tra8brown@gmail.com>
2021-09-04 22:12:03 -0600

    added new files and modified controllers, models, routes  and public folders

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,17 +5,27 @@ const pizzaController = {
     //get all pizzas GET/api/pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
             });
     },
-    //get one pizza by id
+    // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
-                //If no pizza is found, send 404
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
                     return;
