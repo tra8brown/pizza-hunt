cac33571ad649b24f223a1beac556a931697c17c
TraBee <tra8brown@gmail.com>
2021-09-08 12:41:29 -0600

    added validation to model and schema

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -45,7 +45,7 @@ const pizzaController = {
     },
     //update pizza by id //PUT /api/pizzas/:id
     updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
--- models/Pizza.js
+++ models/Pizza.js
@@ -19,6 +19,8 @@ const PizzaSchema = new Schema({
     },
     size: {
         type: String,
+        required: true,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
         default: 'Large'
     },
     toppings: [],
